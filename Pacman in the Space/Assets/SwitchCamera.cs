8a49fa1680fe2bb910e9c2829d7bd013ca00f50c
AlecCross <cross2472@gmail.com>
2021-03-02 22:52:54 +0200

    hw5.1 Зеркало, 5.2 вид от первого лица

---
+++ Pacman in the Space/Assets/SwitchCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    Camera gameCamera;
+    public Camera playerCamera;
+    void Start()
+    {
+        gameCamera = GetComponent<Camera>();
+        gameCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V)){
+            gameCamera.enabled = !gameCamera.enabled;
+            playerCamera.enabled = !playerCamera.enabled;
+        }
+    }
+}
